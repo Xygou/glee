4cd43a596f3b2938596b911ab21604e59247e984
Xygou <xygou1988@gmail.com>
2021-06-08 19:14:55 +0300

    completed the first page

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,19 @@ $(function(){
   $('.top-slider__inner').slick({
     dots:true,
     arrows: false
-  })
+  });
 
-  var mixer = mixitup('.products__items');
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+ 
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
+
 });
+
